a8d710b6b9086e9b4dbdf980e7fcb460de85a42a
Alexander Altotsky <alex@renetusa.com>
2022-02-05 09:43:08 -0800

    Added new VS demo project

---
+++ EntityFrameworkCode/DataContext.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace EntityFrameworkCode
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<>
+
+    }
+    public class Department
+    {
+        // Primary key
+        public int DepartmentId { get; set; }
+        public string? Name { get; set; }
+    }
+
+    public class Employee
+    {
+        // Primary key
+        public int EmployeeId { get; set; }
+
+        public int Title { get; set; }
+
+        // Foreign key
+        public int DepartmentId { get; set; }
+
+        // Foreign key??? - No
+        public int PersonId { get; set; }
+
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+
+        public string? FullName { get; set; }
+    }
+}
